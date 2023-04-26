368c3141e35519260b91ef7e3a7e359261ca84b1
Saroj Ghalan <88360959+sarojghalan@users.noreply.github.com>
2023-04-26 21:41:33 +0545

    cart page completed (#8)

--- src/Routing/Routing.jsx
+++ src/Routing/Routing.jsx
@@ -4,6 +4,7 @@ import Home from "../pages/Home";
 import ProductDetailPage from "../pages/ProductDetailPage";
 import LoginRegisterPage from "../pages/LoginRegisterPage";
 import ProductSearchPage from "../pages/ProductSearchPage";
+import AddToCart from "../components/AddToCart/AddToCart";
 const Routing = () => {
   return (
     <Routes>
@@ -11,6 +12,7 @@ const Routing = () => {
       <Route path="/product_detail" element={<ProductDetailPage />} />
       <Route path="/loginorregister" element={<LoginRegisterPage />} />
       <Route path="/product_search" element={<ProductSearchPage />} />
+      <Route path="/cart" element={<AddToCart />} />
     </Routes>
   );
 };
