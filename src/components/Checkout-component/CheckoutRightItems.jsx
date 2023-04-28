a8e053ff53bc591b9a2da42df76884195b15770a
Subhashish Shah <70791189+subhashishshah@users.noreply.github.com>
2023-04-28 10:31:38 +0545

    Feature/checkoutpage (#11)
    * checkout page done. Responsiveness left
    
    * worked on responisveness

---
+++ src/components/Checkout-component/CheckoutRightItems.jsx
@@ -0,0 +1,173 @@
+import "./CheckoutRightItems.css";
+import Form from "react-bootstrap/Form";
+const CheckoutRightItems = () => {
+  return (
+    <div className="checkout__right__wrapper">
+      <div className="box__heading__title">
+        <p>ORDER SUMMARY</p>
+      </div>
+      <div className="product__item">
+        <img
+          src="https://www.virginmegastore.qa/medias/743242-main.jpg-Thumbnail?context=bWFzdGVyfHJvb3R8NDIzMnxpbWFnZS9qcGVnfGg3ZS9oNDkvOTgzMDg2NjgxMjk1OC83NDMyNDJfX21haW4uanBnX1RodW1ibmFpbHwxODhiMjAxY2Q4Yjk0YTM5ZDdlMjNhYzFlMjhlZWZjYTdhYzIxZWJlYWE5NzgzNjlkNjk3ZmQzZDQ0ZDIzZmE2"
+          alt="iphone"
+          className="product__item__image"
+        />
+        <div className="information__part">
+          <p className="price__info">QAR 119</p>
+          <p className="description__info">
+            QAR 119 HYPHEN AIRE MagSafe Clear Case for iPhone 13 Pro Max
+          </p>
+          <div className="button__wrapper">
+            <div>
+              <button className="cart_quantity">-</button>
+              <button className="cart_quantity__value" disabled>
+                1
+              </button>
+              <button className="cart_quantity">+</button>
+              <span className="qty__font">QTY</span>
+            </div>
+            <div>
+              <span className="delete_icon">
+                <i class="fa-solid fa-trash"></i>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="product__item">
+        <img
+          src="https://www.virginmegastore.qa/medias/743242-main.jpg-Thumbnail?context=bWFzdGVyfHJvb3R8NDIzMnxpbWFnZS9qcGVnfGg3ZS9oNDkvOTgzMDg2NjgxMjk1OC83NDMyNDJfX21haW4uanBnX1RodW1ibmFpbHwxODhiMjAxY2Q4Yjk0YTM5ZDdlMjNhYzFlMjhlZWZjYTdhYzIxZWJlYWE5NzgzNjlkNjk3ZmQzZDQ0ZDIzZmE2"
+          alt="iphone"
+          className="product__item__image"
+        />
+        <div className="information__part">
+          <p className="price__info">QAR 119</p>
+          <p className="description__info">
+            QAR 119 HYPHEN AIRE MagSafe Clear Case for iPhone 13 Pro Max
+          </p>
+          <div className="button__wrapper">
+            <div>
+              <button className="cart_quantity">-</button>
+              <button className="cart_quantity__value" disabled>
+                1
+              </button>
+              <button className="cart_quantity">+</button>
+              <span className="qty__font">QTY</span>
+            </div>
+            <div>
+              <span className="delete_icon">
+                <i class="fa-solid fa-trash"></i>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="product__item">
+        <img
+          src="https://www.virginmegastore.qa/medias/743242-main.jpg-Thumbnail?context=bWFzdGVyfHJvb3R8NDIzMnxpbWFnZS9qcGVnfGg3ZS9oNDkvOTgzMDg2NjgxMjk1OC83NDMyNDJfX21haW4uanBnX1RodW1ibmFpbHwxODhiMjAxY2Q4Yjk0YTM5ZDdlMjNhYzFlMjhlZWZjYTdhYzIxZWJlYWE5NzgzNjlkNjk3ZmQzZDQ0ZDIzZmE2"
+          alt="iphone"
+          className="product__item__image"
+        />
+        <div className="information__part">
+          <p className="price__info">QAR 119</p>
+          <p className="description__info">
+            QAR 119 HYPHEN AIRE MagSafe Clear Case for iPhone 13 Pro Max
+          </p>
+          <div className="button__wrapper">
+            <div>
+              <button className="cart_quantity">-</button>
+              <button className="cart_quantity__value" disabled>
+                1
+              </button>
+              <button className="cart_quantity">+</button>
+              <span className="qty__font">QTY</span>
+            </div>
+            <div>
+              <span className="delete_icon">
+                <i class="fa-solid fa-trash"></i>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="product__item">
+        <img
+          src="https://www.virginmegastore.qa/medias/743242-main.jpg-Thumbnail?context=bWFzdGVyfHJvb3R8NDIzMnxpbWFnZS9qcGVnfGg3ZS9oNDkvOTgzMDg2NjgxMjk1OC83NDMyNDJfX21haW4uanBnX1RodW1ibmFpbHwxODhiMjAxY2Q4Yjk0YTM5ZDdlMjNhYzFlMjhlZWZjYTdhYzIxZWJlYWE5NzgzNjlkNjk3ZmQzZDQ0ZDIzZmE2"
+          alt="iphone"
+          className="product__item__image"
+        />
+        <div className="information__part">
+          <p className="price__info">QAR 119</p>
+          <p className="description__info">
+            QAR 119 HYPHEN AIRE MagSafe Clear Case for iPhone 13 Pro Max
+          </p>
+          <div className="button__wrapper">
+            <div>
+              <button className="cart_quantity">-</button>
+              <button className="cart_quantity__value" disabled>
+                1
+              </button>
+              <button className="cart_quantity">+</button>
+              <span className="qty__font">QTY</span>
+            </div>
+            <div>
+              <span className="delete_icon">
+                <i class="fa-solid fa-trash"></i>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="product__item">
+        <img
+          src="https://www.virginmegastore.qa/medias/743242-main.jpg-Thumbnail?context=bWFzdGVyfHJvb3R8NDIzMnxpbWFnZS9qcGVnfGg3ZS9oNDkvOTgzMDg2NjgxMjk1OC83NDMyNDJfX21haW4uanBnX1RodW1ibmFpbHwxODhiMjAxY2Q4Yjk0YTM5ZDdlMjNhYzFlMjhlZWZjYTdhYzIxZWJlYWE5NzgzNjlkNjk3ZmQzZDQ0ZDIzZmE2"
+          alt="iphone"
+          className="product__item__image"
+        />
+        <div className="information__part">
+          <p className="price__info">QAR 119</p>
+          <p className="description__info">
+            QAR 119 HYPHEN AIRE MagSafe Clear Case for iPhone 13 Pro Max
+          </p>
+          <div className="button__wrapper">
+            <div>
+              <button className="cart_quantity">-</button>
+              <button className="cart_quantity__value" disabled>
+                1
+              </button>
+              <button className="cart_quantity">+</button>
+              <span className="qty__font">QTY</span>
+            </div>
+            <div>
+              <span className="delete_icon">
+                <i class="fa-solid fa-trash"></i>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="coupon__wrapper">
+        <Form.Group controlId="formBasicEmail">
+          <Form.Control
+            type="email"
+            className="form__input__filed"
+            placeholder="Enter your coupons"
+          />
+        </Form.Group>
+        <button className="coupons__button">APPLY</button>
+      </div>
+      <div className="pricing__wrapper">
+        <div className="subtotal__wrapper">
+          <p className="pricing__title">SUBTOTAL</p>
+          <p className="actual__price">Rs. 8,996</p>
+        </div>
+        <div className="total">
+          <p className="pricing__title">TOTAL</p>
+          <p className="actual__price">Rs. 8,996</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CheckoutRightItems;
